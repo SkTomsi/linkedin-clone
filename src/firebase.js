1bc04b082fe067de189409e27ae54cf7c731bcbb
TomcyT <tomcy48@gmail.com>
2022-01-24 14:21:38 +0530

    added firebase connectivity

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDyACvowyVY5AH36-l8-kbNLzOou3TIUq8",
+  authDomain: "linkedin-clone-ba21b.firebaseapp.com",
+  projectId: "linkedin-clone-ba21b",
+  storageBucket: "linkedin-clone-ba21b.appspot.com",
+  messagingSenderId: "134703304751",
+  appId: "1:134703304751:web:f1487d7f25f0620ec976da",
+  measurementId: "G-ZQ21CJMX76",
+};
